a40620b44095025429d8bf519a237a1b4fcfd56e
Eugeniy Kalinin <burump@gmail.com>
2011-05-08 20:37:03 -0700

    Renamed Game1 into Game

--- Source/Program.cs
+++ Source/Program.cs
@@ -10,7 +10,7 @@ namespace Raven
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (Game game = new Game())
             {
                 game.Run();
             }
