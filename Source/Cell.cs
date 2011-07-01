fabacd0de72a48c18738131bd512a5705798b5bd
Eugeniy Kalinin <burump@gmail.com>
2011-07-01 16:00:53 -0700

    removed the planet and added a cell class

---
+++ Source/Cell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Raven
+{
+    public class Cell
+    {
+        public Point Position { get; set; }
+
+        public Point[] Neighbors
+        {
+            get
+            {
+                return new Point[] {
+                    new Point(Position.X, Position.Y + 1),
+                    new Point(Position.X + 1, Position.Y),
+                    new Point(Position.X, Position.Y - 1),
+                    new Point(Position.X - 1, Position.Y)
+                };
+            }
+        }
+
+        public Point[] Corners
+        {
+            get
+            {
+                return new Point[] {
+                    new Point(Position.X + 1, Position.Y + 1),
+                    new Point(Position.X + 1, Position.Y),
+                    new Point(Position.X, Position.Y),
+                    new Point(Position.X, Position.Y + 1)
+                };
+            }
+        }
+    }
+}
